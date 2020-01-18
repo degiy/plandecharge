b6f8fe9425b2dc3aaf3f20eeca48dd29e13f1c40
degiy <degiy@yahoo.fr>
2020-01-19 00:52:07 +0100

    gen ressources.csv

--- gen.pl
+++ gen.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use List::Util qw(sum);
+
 $verbose=1;
 
 # generation des onglets du plan de charge en fonction de la feuille config exportée en csv par le vba
@@ -31,7 +33,7 @@ while(<F>)
 	shift @mres;
 	$res=shift @mres;
 	push @tres,$res;
-	$htmres{$res}=@mres;
+	$htmres{$res}=[@mres];
 	$htares{$res}=[];
 	print "ressource $res : ",join (',',@mres),"\n" if $verbose;
     }
@@ -71,6 +73,58 @@ while(<F>)
 }
 close F;
 
-# generation du fichier csv des ressources
+# post traitements
+@tres=sort @tres;
+@tact=sort @tact;
+$nbmois=$#mois+1;
+@zm=(0)x$nbmois;
 
+@xlet=split /,/,"A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,AX,AY,AZ";
 
+# generation du fichier csv des ressources (celui ou on va choisir combien la ressource R impute chaque mois sur ces differentes activites)
+open (F,">ressources.csv") or die;
+$y=1;
+print F "mois,",join(',',@mois),",total\n\n";
+$y+=2;
+foreach $r (@tres)
+{
+    @max=@{$htmres{$r}};
+    $tot=sum @max;
+    print F "$r,",join(',',@max),",$tot\n";
+    $y++;
+    $y0=$y;
+    foreach $a (@{$htares{$r}})
+    {
+	print F "$a,",join(',',@zm),",=SOMME(B$y:",$xlet[$nbmois],$y,")\n";
+	$hty{$a}{$r}=$y;
+	$y++;	
+    }
+    print F "total(j)";
+    for($x=1;$x<=$nbmois+1;$x++)
+    {
+	$let=$xlet[$x];
+	print F ",=SOMME($let$y0:$let",$y-1,")";
+    }
+    print F "\n";
+    $y++;
+    print F "total(%)";
+    for($x=1;$x<=$nbmois+1;$x++)
+    {
+	$let=$xlet[$x];
+	print F ",=$let",$y-1,"/$let",$y0-1;
+    }
+    print F "\n";
+    $y++;
+	
+    print F "\n";
+    $y++;
+}
+close F;
+
+# generation du fichier csv des ressources (celui ou on va choisir combien la ressource R impute chaque mois sur ces differentes activites)
+open (F,">activites.csv") or die;
+$y=1;
+print F "BC,code,activite,ressource,",join(',',@mois),",total(j),total(%)\n\n";
+$y+=2;
+
+close F;
