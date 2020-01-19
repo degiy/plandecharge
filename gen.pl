81238dad1631926697f557de1779c6d76eb620ef
degiy <degiy@yahoo.fr>
2020-01-19 20:15:07 +0100

    premiere version utilisable, reload de charge pas encore code mais prepare

--- gen.pl
+++ gen.pl
@@ -9,6 +9,7 @@ $verbose=1;
 @tres=();
 @tact=();
 @files=();
+$reload=0;
 
 # lecture de la config
 open (F,'config.csv') or die "pas de fichier config.csv a lire";
@@ -52,17 +53,13 @@ while(<F>)
 	$htact{$act}{'hjtot'}=shift @t;
 	$htact{$act}{'hjdjc'}=shift @t;
 	@qui=sort @t;
-	@qui2=();
 	foreach $r (@qui)
 	{
 	    unless ($r eq '')
 	    {
 		push @{$htares{$r}},$act;
-		push @qui2,$r;
 	    }
 	}
-	$htact{$act}{'qui'}=@qui2;
-	print "activite $act : ",join(',',@qui2),"\n" if $verbose;
     }
     # fichier supplementaire a charger sous forme d'onglet suppl.
     elsif (/^F/)
@@ -81,6 +78,39 @@ $nbmois=$#mois+1;
 
 @xlet=split /,/,"A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,AX,AY,AZ";
 
+# faut-il recharger les charges declarees pour les ressource d'un tableur precedent ?
+goto gen unless  ($#ARGV==0) && ( $ARGV[0] eq '-r' );
+
+open(F,"saved_ressources.csv") or die;
+$reload=1;
+while (<F>)
+{
+    s/\r//gs;
+    s/\n//gs;
+    # liste des mois
+    if (/^mois,.*,total/)
+    {
+	# recup de la ligne des mois (par forcement les memes qu'on a dans le nouveau plan de charge)
+	@omois=split /,/,$_;
+	shift @omois; pop @omois;
+	print "old mois : ",join(',',@omois),"\n" if $verbose;
+    }
+    elsif (/^,*$/)
+    {
+	# ligne vide (donc nouvelle ressource, pas forcement presente dans le nouveau plan de charge)
+    }
+    elsif (/^total/)
+    {
+	# fin du bloc de conso
+    }
+    else
+    {
+    }
+}
+
+close F;
+
+ gen: ;
 # generation du fichier csv des ressources (celui ou on va choisir combien la ressource R impute chaque mois sur ces differentes activites)
 open (F,">ressources.csv") or die;
 $y=1;
@@ -95,7 +125,21 @@ foreach $r (@tres)
     $y0=$y;
     foreach $a (@{$htares{$r}})
     {
-	print F "$a,",join(',',@zm),",=SOMME(B$y:",$xlet[$nbmois],$y,")\n";
+	print F "$a,";
+	if ($reload)
+	{
+	    for($x=0;$x<$nbmois;$x++)
+	    {
+		$charge=0;
+		$charge=$htreload{$r}{$a}{$mois[$x]} if exists $htreload{$r}{$a}{$mois[$x]};
+		print F "$charge,";
+	    }
+	}
+	else
+	{
+	    print F join(',',@zm);
+	}
+	print F ",=SOMME(B$y:",$xlet[$nbmois],$y,")\n";
 	$hty{$a}{$r}=$y;
 	$y++;	
     }
@@ -113,18 +157,52 @@ foreach $r (@tres)
 	$let=$xlet[$x];
 	print F ",=$let",$y-1,"/$let",$y0-1;
     }
-    print F "\n";
-    $y++;
-	
-    print F "\n";
-    $y++;
+    print F "\n\n";
+    $y+=2;
 }
 close F;
 
-# generation du fichier csv des ressources (celui ou on va choisir combien la ressource R impute chaque mois sur ces differentes activites)
+# generation du fichier csv des activites (recap des estimations de conso par rapport au volume de l'activite)
 open (F,">activites.csv") or die;
 $y=1;
-print F "BC,code,activite,ressource,",join(',',@mois),",total(j),total(%)\n\n";
-$y+=2;
-
+foreach $a (@tact)
+{
+    # les ressource sur l'activite
+    @res=sort keys %{$hty{$a}};
+    $nbres=$#res+1;
+    # info generales sur l'activite
+    print F "activite,BC,code,deb,fin,total(j),deja(j),rafi(j),raf(j),raf(%)\n";
+    $y++;
+    $raf=$htact{$a}{'hjtot'}-$htact{$a}{'hjdjc'};
+    print F "$a,",$htact{$a}{'BC'},',',$htact{$a}{'code'},',',$htact{$a}{'mdeb'},',',$htact{$a}{'mfin'},',',$htact{$a}{'hjtot'},',',$htact{$a}{'hjdjc'},',',$raf,',';
+    # raf j et % (par rapport au tableau de la conso ressource juste en dessous)
+    print F "=H$y-",$xlet[$nbmois+1],$y+$nbres+2,",=I$y/H$y\n";
+    $y++;
+    print F "ressource,",join(',',@mois),",total(j),total(%)\n";
+    $y++;
+    $y1=$y;
+    # pour chaque ressource, recopie des imputs de la feuille des ressources
+    foreach $r (@res)
+    {
+	print F "$r";
+	for($x=1;$x<=$nbmois+1;$x++)
+	{
+	    $let=$xlet[$x];
+	    $y0=$hty{$a}{$r};
+	    print F ",=ressources!$let$y0";
+	}
+	# pct du raf initial
+	print F ",=$let$y/H",$y1-2,"\n";
+	$y++;	
+    }
+    # total
+    print F "total(j)";
+    for($x=1;$x<=$nbmois+2;$x++)
+    {
+	$let=$xlet[$x];
+	print F ",=SOMME($let$y1:$let",$y-1,")";
+    }
+    print F "\n\n";
+    $y+=2;
+}
 close F;
